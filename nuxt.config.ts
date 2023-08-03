9361495c1e6ad63e1d84b82a4387536295ce66b3
Damir Mandakovic <damir@notus.cl>
2023-08-02 22:23:14 -0400

    🎨 prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,34 @@
-const apiAddress = process.env.NODE_ENV === 'development' ? 'http://localhost:8000/djangoapi/' : 'https://andinismodeescritorio.cl/djangoapi/'
+const apiAddress =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8000/djangoapi/"
+    : "https://andinismodeescritorio.cl/djangoapi/";
 
 export default defineNuxtConfig({
   runtimeConfig: {
     // The private keys which are only available server-side
-    apiSecret: '123',
+    apiSecret: "123",
     // Keys within public are also exposed client-side
     public: {
       apiBase: apiAddress,
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
-    }
+      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    },
     // These variables are exposed to the rest of your application using the useRuntimeConfig composable.
   },
   // app: {
   //   pageTransition: { name: 'page', mode: 'out-in' }
   // },
   build: {
-    transpile: ['@googlemaps/js-api-loader'],
+    transpile: ["@googlemaps/js-api-loader"],
   },
   css: ["@/assets/styles/main.scss"],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "@/assets/styles/_variables.scss";'
-        }
-      }
-    }
+          additionalData: '@import "@/assets/styles/_variables.scss";',
+        },
+      },
+    },
   },
-  modules: [
-    '@nuxt/content'
-  ],
-})
+  modules: ["@nuxt/content"],
+});
