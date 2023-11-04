548de245677d604b2db9520d76dbc77440075e7e
Damir Mandakovic <damir@notus.cl>
2023-11-04 22:03:38 +0100

    ✨ New Blog Index Style

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,31 @@
 <template>
   <div class="blog-index">
-    <MainstructureTitleSection name="Blog" />
     <div class="blog-cards">
-      <ContentList path="/blog" v-slot="{ list }">
+      <ContentList :query="queryParamsFirst" v-slot="{ list }">
+        <div
+          v-for="article in list"
+          :key="article._path"
+          class="blog-card blog-card-wide"
+        >
+          <div class="blog-card-header">
+            <img
+              :src="
+                image_urls.find((image) => image.id === article.mainimageid)
+                  .main_image
+              "
+              alt="blog image"
+            />
+            <NuxtLink :to="article._path">
+              <h2>{{ article.title }}</h2>
+            </NuxtLink>
+          </div>
+          <NuxtLink :to="'/andinistas/' + article.authorid">
+            <p>{{ article.author }}</p>
+          </NuxtLink>
+          <p class="datespanish">{{ article.datespanish }}</p>
+        </div>
+      </ContentList>
+      <ContentList :query="queryParamsRest" v-slot="{ list }">
         <div v-for="article in list" :key="article._path" class="blog-card">
           <div class="blog-card-header">
             <img
@@ -20,7 +43,6 @@
             <p>{{ article.author }}</p>
           </NuxtLink>
           <p class="datespanish">{{ article.datespanish }}</p>
-          <p>{{ article.description }}</p>
         </div>
       </ContentList>
     </div>
@@ -28,18 +50,39 @@
 </template>
 
 <script setup lang="ts">
-const runtimeConfig = useRuntimeConfig();
+const config = useRuntimeConfig();
 
 // save mainimageid from every article of content NUXT
 const contents = await queryContent("blog").only("mainimageid").find();
 // const image_ids = contents.map((content) => content.mainimageid)
 
+// Query for the List object
+import type { QueryBuilderParams } from "@nuxt/content/dist/runtime/types";
+const queryParamsFirst: QueryBuilderParams = {
+  path: "/blog",
+  where: { draft: false },
+  sort: { date: -1 },
+  limit: 1, // Limit the results to only one post
+};
+
+// Define the query parameters to get all blog posts but skip the first
+const queryParamsRest: QueryBuilderParams = {
+  path: "/blog",
+  where: { draft: false },
+  sort: { date: -1 },
+  skip: 1, // Skip the first result
+};
+
 const image_urls = await Promise.all(
   contents.map(async (article) => {
     const apiURLImage =
-      runtimeConfig.public.apiBase + "image/" + article.mainimageid + "/";
+      config.public.apiBase + "image/" + article.mainimageid + "/";
     const { data } = await useFetch(apiURLImage);
-    return { id: article.mainimageid, image: data.value.tb_item_cover };
+    return {
+      id: article.mainimageid,
+      image: data.value.tb_item_cover,
+      main_image: data.value.image,
+    };
   })
 );
 </script>
@@ -62,17 +105,18 @@ const image_urls = await Promise.all(
       border-radius: 0 0 20px 20px;
       background-color: $color-light;
       padding-bottom: 15px;
-      -webkit-box-shadow: 4px 4px 18px -13px rgba(0, 0, 0, 0.42);
-      -moz-box-shadow: 4px 4px 18px -13px rgba(0, 0, 0, 0.42);
-      box-shadow: 4px 4px 18px -13px rgba(0, 0, 0, 0.42);
+      -webkit-box-shadow: 10px 12px 18px -10px rgba(0, 0, 0, 0.42);
+      -moz-box-shadow: 10px 12px 18px -10px rgba(0, 0, 0, 0.42);
+      box-shadow: 10px 12px 18px -10px rgba(0, 0, 0, 0.42);
       .blog-card-header {
         position: relative;
         img {
           //height: 200px;
           width: 100%;
+          height: 300px;
           border-radius: 11px 11px 0 0;
           object-fit: cover;
-          filter: brightness(80%);
+          filter: brightness(70%) hue-rotate(-15deg);
         }
         h2 {
           //transform: translateY(-110%);
@@ -102,7 +146,7 @@ const image_urls = await Promise.all(
         }
       }
       p {
-        font-weight: 200;
+        font-weight: 600;
         font-family: "Lora", serif;
         font-size: 0.9em;
       }
@@ -112,6 +156,20 @@ const image_urls = await Promise.all(
         color: $color-main-second;
       }
     }
+    .blog-card-wide {
+      width: 80%;
+      .blog-card-header {
+        img {
+          height: auto;
+          max-height: 500px;
+          object-position: top;
+        }
+        h2 {
+          font-size: 3rem;
+          line-height: 1.3em;
+        }
+      }
+    }
   }
 }
 @media screen and (max-width: 768px) {
@@ -119,6 +177,15 @@ const image_urls = await Promise.all(
     .blog-cards {
       .blog-card {
         width: 90%;
+      }
+      .blog-card-wide {
+        width: 90%;
+        .blog-card-header {
+          h2 {
+            font-size: 1.5rem !important;
+            line-height: inherit !important;
+          }
+        }
       }
     }
   }
